745687dbff537a81474ab565ea2099f05fc5e3c9
Yusuf <Yusufsalih4@gmail.com>
2022-09-26 15:20:12 +0530

    create shopping.api project and setting data, controllers, models

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace Shopping.API.Controllers
+
+
+    //api controller over here
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ProductController
+    {
+        //creating the logger object
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        //get method to get the data from api
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
