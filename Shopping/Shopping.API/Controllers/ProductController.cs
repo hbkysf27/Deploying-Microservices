47b37897220867a7eef647357b127a88dc43dfeb
Yusuf <Yusufsalih4@gmail.com>
2022-10-04 10:28:03 +0530

    connec mongo container from shopping api microservice, refactor product context object

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using Shopping.API.Data;
 using Shopping.API.Models;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
@@ -13,21 +14,32 @@ namespace Shopping.API.Controllers
     [Route("[Controller]")]
     public class ProductController
     {
+
+        private readonly ProductContext _context;
         //creating the logger object
 
         private readonly ILogger<ProductController> _logger;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
         {
-            _logger = logger;
+
+                       
+
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger= logger ?? throw new ArgumentNullException(nameof(logger));
         }
+
+
 
         //get method to get the data from api
 
         [HttpGet]
-        public IEnumerable<Product> Get()
+        public async Task<IEnumerable<Product>> Get()
         {
-            return ProductContext.Products;
+            return await _context
+                         .Products
+                         .Find(p=>true)
+                         .ToListAsync();
         }
     }
 }
